19fffea0a7114da95a8dfbc170dff2fead33aa23
damnvaibhav <vaibhavsjadhav9@gmail.com>
2022-10-04 10:36:18 +0530

    Increment & Decrement Operator

---
+++ program_04.c
@@ -0,0 +1,27 @@
+/*
+Program Name : Increment & Decrement Operator
+
+Date & Time : 4 - 10 - 2022 // 10:30am
+
+Author : Vaibhav Jadhav
+*/
+
+/* program start */
+
+/* main method */
+#include <stdio.h>
+
+int main()
+{
+    /* dataTypes & variables */
+    int a = 10;
+    int b = 10;
+
+    // Process & Output
+    printf("a: %d", a++); // a = 11
+    printf("a: %d", ++a); // a = 12
+    printf("a: %d", a++); // a = 12
+
+    return 0;
+    /* program end */
+}
