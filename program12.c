79c802f23c88313260524d5377443050617b998d
damnvaibhav <vaibhavsjadhav9@gmail.com>
2022-10-29 11:59:45 +0530

    for loop example

---
+++ program12.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+
+    for (int i = 1; i <= 1; i++)
+    {
+        for (int j = 10; j >= 1; j--)
+        {
+            printf("\n%d\t%d", i, j);
+            i++;
+        }
+    }
+    return 0;
+}
